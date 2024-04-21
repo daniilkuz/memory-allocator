1c0ce2610d1aff03924245d7351eeed25c18c25b
--- <--->
2024-04-22 03:56:24 +0700

    initial logic in malloc function

--- memory-allocator.c
+++ memory-allocator.c
@@ -11,3 +11,36 @@ union header{
 };
 
 typedef union header Header;
+
+
+static Header base;
+static Header *freep = NULL;
+
+void malloc(unsigned nbytes){
+
+	Header *p, *prevp;
+	Header *morecore(unsigned);
+	unsigned nunits;
+
+	nunits = (nbytes+sizeof(Header)-1)/sizeof(Header) + 1;
+	if((prevp=freep)==NULL){
+		base.s.ptr = freep = prevp = &base;
+		base.s.size = 0;
+	}
+
+	for(p = prevp->s.ptr; ; prevp=p, p=p->s.ptr){
+		if(p->s.size>=nunits){
+			if(p->p.size==nunits){
+				prevp->s.ptr = p->s.ptr;
+			} else {
+				p->s.size-=nunits;
+				p += p->s.size;
+				p->s.size = nunits;
+			}
+			freep=prevp;
+			return (void *) (p+1);
+		}
+	}
+
+}
+
